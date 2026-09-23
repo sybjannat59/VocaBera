c1e8ca6e447b2b0211e9dc95a1d26aaa03405631
sybjannat59 <shoayebjannat39@gmail.com>
2026-09-23 23:29:48 +0600

    Delete src directory

--- src/app/offline/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-import type { ReactNode } from "react";
-
-export const metadata: Metadata = { title: "Offline", robots: { index: false } };
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return children;
-}
